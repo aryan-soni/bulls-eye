18ef899b6d4993bcfebb653f127f2058ea2522a5
Aryan Soni <mail.aryansoni@gmail.com>
2020-12-19 16:23:53 -0500

    add functionality to caclulate annual returns to Stock class, and ensure that standard deviation calculations in the Calculator class use the annual returns data. Furthermore, implement functionality in Calculator to calculate the stock's Sharpe ratio, leveraging the annual returns again.

--- main.py
+++ main.py
@@ -22,11 +22,17 @@ Attributes:
     years_of_data: The number of years of data that the stock has.
     total_return: The stock's five-year total return (including dividends, but assuming 
         the dvidends aren't re-invested).
-    total_returns: A list containing the stock's total returns month-over-month
+    total_monthly_returns: A list containing the stock's total returns month-over-month
         for 5 years.
-    mean: The stock's mean return over 5 years.
-    deviations: A list containing the deviations between the stock's
-        month-over-month returns over 5 years and the stock's mean return over
+    total_annual_returns: A list containing the stock's total returns year-over-year
+        for 5 years.
+    mean_monthly_return: The stock's mean return over 5 years.
+    mean_annual_return: The stock's mean annual return over 5 years.
+    deviations_monthly_returns: A list containing the deviations between the stock's
+        month-over-month returns over 5 years and the stock's mean monthly return over
+        5 years.
+    deviations_annual_returns: A list containing the deviations between the stock's
+        annual returns over 5 years and the stock's mean annual return over
         5 years.
 """
 
@@ -41,34 +47,55 @@ Attributes:
         self.months_of_data = len(list(self.stock_data.keys())) - 1
         self.years_of_data = self.months_of_data // 12
 
-        self.total_returns = []
-        self.deviations = []
+        self.total_monthly_returns = []
+        self.total_annual_returns = []
+        self.deviations_monthly_returns = []
+        self.deviations_annual_returns = []
 
-        # Populate returns list for stock.
+        # Populate monthly returns list for stock.
         if self.years_of_data >= 5:
             for i in range(60):
-                self.total_returns.append(self.calculate_month_return(i))
+                self.total_monthly_returns.append(self.calculate_month_return(i))
         else:
             for i in range(self.months_of_data):
-                self.total_returns.append(self.calculate_month_return(i))
+                self.total_monthly_returns.append(self.calculate_month_return(i))
+
+        # Populate annual returns list for stock.
+        if self.years_of_data >= 5:
+            for i in range(5):
+                self.total_annual_returns.append(self.calculate_year_return(i * 12 + 1))
+        else:
+            for i in range(self.months_of_data // 12):
+                self.total_annual_returns.append(self.calculate_year_return(i * 12 + 1))
 
         # Determine total return over 5 years
         self.total_return = self.calculate_total_return()
 
-        self.mean = 0
+        self.mean_monthly_return = 0
+        self.mean_annual_return = 0
 
         if self.years_of_data >= 5:
-            self.mean = sum(self.total_returns) / 60
+            self.mean_monthly_return = sum(self.total_monthly_returns) / 60
+            self.mean_annual_return = sum(self.total_annual_returns) / 5
         else:
-            self.mean = sum(self.total_returns) / self.months_of_data
+            self.mean_monthly_return = sum(self.total_monthly_returns) / self.months_of_data
+            self.mean_annual_return = sum(self.total_monthly_returns) / (self.months_of_data // 12)
 
-        # Populate deviations list for stock.
+        # Populate monthly deviations lists for stock.
         if self.years_of_data >= 5:
             for i in range(60):
-                self.deviations.append(self.total_returns[i] - self.mean)
+                self.deviations_monthly_returns.append(self.total_monthly_returns[i] - self.mean_monthly_return)
         else:
             for i in range(self.months_of_data):
-                self.deviations.append(self.total_returns[i] - self.mean)
+                self.deviations_monthly_returns.append(self.total_monthly_returns[i] - self.mean_monthly_return)
+
+        # Populate annual deviations lists for stock.
+        if self.years_of_data >= 5:
+            for i in range(5):
+                self.deviations_annual_returns.append(self.total_annual_returns[i] - self.mean_annual_return)
+        else:
+            for i in range(self.months_of_data // 12):
+                self.deviations_annual_returns.append(self.total_annual_returns[i] - self.mean_annual_return)
 
     def calculate_month_return(self, end_index=1):
         """ Determines a stock's total return (%) for a given month
@@ -87,7 +114,7 @@ Attributes:
                 complete historical data.
 
         Returns:
-            The actual rate of return for a given month (%)
+            The actual rate of return for a given month (%).
         """
 
         end_index = end_index
@@ -104,6 +131,52 @@ Attributes:
             self.stock_data[end_key]["5. adjusted close"])
         start_price = float(self.stock_data[start_key]["5. adjusted close"]) - float(
             self.stock_data[start_key]["7. dividend amount"])
+
+        total_returns = (
+            (adjusted_end_price - start_price) / start_price) * 100
+
+        return total_returns
+
+    def calculate_year_return(self, end_index=1):
+        """ Determines a stock's total return (%) for a given year
+
+        Args:
+            end_index: Where the parent keys in self.stock_data are converted
+                to a list, end_index represents which index the "end month"
+                would be. The "end month" is the month which you use to
+                calculate the "end price." For example, if you were to calculate
+                your returns from April 2019-April 2020, your end month would be
+                March 2020, as you would look at the data from the end of March 2020 to
+                calculate the "end price." In this context. the "start month"
+                would be March 2019, as you would look to the data from the end of March
+                2019 to calculate the "start price." Moving on, the end_index is set
+                to 1 by default as index 1 will store the most recent month with
+                complete historical data.
+
+        Returns:
+            The actual rate of return for a given year (%).
+        """
+
+        end_index = end_index
+        start_index = end_index + 12 # Start index is 12 months before the end_index
+
+        # Isolate dates corresponding to end and start months, which will be a
+        # string that will be used as a key.
+        end_key = list(self.stock_data.keys())[end_index]
+        start_key = list(self.stock_data.keys())[start_index]
+
+        # Isolate adjusted price for the end of the end month, and the initial price
+        # from the end of the start month (ensure dividend is removed).
+        adjusted_end_price = float(
+            self.stock_data[end_key]["5. adjusted close"])
+        start_price = float(self.stock_data[start_key]["5. adjusted close"]) - float(
+            self.stock_data[start_key]["7. dividend amount"])
+
+        # Add dividends from entire year to end price
+        for i in range(end_index + 1, start_index):
+            selected_key = list(self.stock_data.keys())[i]
+            adjusted_end_price += float(
+                self.stock_data[selected_key]["7. dividend amount"])
 
         total_returns = (
             (adjusted_end_price - start_price) / start_price) * 100
@@ -158,8 +231,10 @@ Attributes:
     risk_free_return: The risk-free rate of return.
     alpha: The alpha of the stock (specificially Jensen's alpha, as per the Capital Asset Pricing Model).
     r-squared: The r-squared of the stock when comparing it to SPY (the benchmark).
-    standard_deviation: The standard deviation of the chosen stock, reflecting the spread
+        standard_deviation: The standard deviation of the chosen stock, reflecting the spread
         of returns relative to the mean return.
+    sharpe_ratio: The sharpe ratio of the chosen stock, reflecting the average return earned in excess 
+        of the risk-free rate per unit of volatility.
 """
 
     def __init__(self, stock, index):
@@ -175,6 +250,7 @@ Attributes:
         self.alpha = self.calculate_alpha()
         self.r_squared = self.calculate_r_squared()
         self.standard_deviation = self.calculate_standard_deviation()
+        self.sharpe_ratio = self.calculate_sharpe_ratio()
 
     def calculate_covariance(self):
         """ Determines the covariance of the chosen stock and index.
@@ -184,7 +260,7 @@ Attributes:
         """
 
         product_of_deviations = [
-            a * b for a, b in zip(self.stock.deviations, self.index.deviations)]
+            a * b for a, b in zip(self.stock.deviations_monthly_returns, self.index.deviations_monthly_returns)]
 
         sum_of_products = sum(product_of_deviations)
 
@@ -200,7 +276,7 @@ Attributes:
             The variance of index.
         """
 
-        squared_deviations = [(n) ** 2 for n in self.index.deviations]
+        squared_deviations = [(n) ** 2 for n in self.index.deviations_monthly_returns]
 
         sum_of_squared_deviations = sum(squared_deviations)
 
@@ -265,16 +341,16 @@ Attributes:
                 stock's movements correlate with the benchmark index's (SPY).
         """
 
-        product_of_returns = [a * b for a, b in zip(self.stock.total_returns,
-                                                    self.index.total_returns)]
+        product_of_returns = [a * b for a, b in zip(self.stock.total_monthly_returns,
+                                                    self.index.total_monthly_returns)]
 
-        sum_of_stock_returns = sum(self.stock.total_returns)
-        sum_of_index_returns = sum(self.index.total_returns)
+        sum_of_stock_returns = sum(self.stock.total_monthly_returns)
+        sum_of_index_returns = sum(self.index.total_monthly_returns)
 
         sum_of_stock_returns_squared = sum(
-            [(n**2) for n in self.stock.total_returns])
+            [(n**2) for n in self.stock.total_monthly_returns])
         sum_of_index_returns_squared = sum(
-            [(n**2) for n in self.index.total_returns])
+            [(n**2) for n in self.index.total_monthly_returns])
 
         # Split calculations for numerator and denominator for readability
         if self.stock.years_of_data >= 5:
@@ -293,21 +369,36 @@ Attributes:
 
         Returns:
             The standard deviation of the chosen stock, reflecting the spread
-            of returns relative to the mean return.
+                of the stock's returns relative to the mean return.
         """
 
         if self.stock.years_of_data >= 5:
-            return (sum([(n ** 2) for n in self.stock.deviations]) / 59) ** 0.5
+            return (sum([(n ** 2) for n in self.stock.deviations_annual_returns]) / 4) ** 0.5
         else:
-            return (sum([(n ** 2) for n in self.stock.deviations]) / (self.stock.months_of_data - 1)) ** 0.5
+            return (sum([(n ** 2) for n in self.stock.deviations_annual_returns]) / ((self.stock.months_of_data // 12) - 1)) ** 0.5
+
+
+    def calculate_sharpe_ratio(self):
+        """ Determines the sharpe ratio of the chosen stock.
+
+        Returns:
+            The sharpe ratio of the chosen stock, reflecting the average return earned in excess 
+                of the risk-free rate per unit of volatility.
+        """
+
+        # For any given year, the risk-free rate can be divided by 5 since it describes a 5y period
+        # Divide risk-free rate by 5 as you need the rate on a per year basis to estimate the excess return per year
+        risk_free_rate = self.risk_free_return / 5
+
+        return (self.stock.mean_annual_return - risk_free_rate) / self.standard_deviation
 
 stock = Stock(data)
 index = Stock(index_data)
 calculator = Calculator(stock, index)
 
-
-print(len(calculator.stock.total_returns))
+print(len(calculator.stock.total_monthly_returns))
 print(round(calculator.beta, 2))
 print(round(calculator.alpha, 2))
 print(round(calculator.r_squared, 2))
 print(round(calculator.standard_deviation, 2))
+print(round(calculator.sharpe_ratio, 2))
